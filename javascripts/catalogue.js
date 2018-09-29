159e892a4bc62ffd6362f75acf5431b8b4d927cf
ReadingWithAusten <ngd253@usask.ca>
2018-09-29 04:08:22 -0400

    Beta 2.03: Tokenization of Catalogue Queries

--- javascripts/catalogue.js
+++ javascripts/catalogue.js
@@ -12,6 +12,7 @@
   var number_displayed_results = 18;
   var category = "";
   var query = "";
+  var query_tokens = [];
   var query_result = [];
 
   document.addEventListener('keypress', function(event){
@@ -35,45 +36,83 @@
   function checkBookAttributes(book) {
       var keys = Object.keys(COMPLETE_DATA[book]);
       var result = false;
-      keys.forEach(function(k){
-        if(k == "date_published"){
-          result = queryDatePublished(COMPLETE_DATA[book], query);
-        }else if (COMPLETE_DATA[book][k].toString().toLowerCase().indexOf(query.toLowerCase()) > -1) {
-          result =  true;
+      keys.forEach(function(key){
+        if(category == "date_published"){
+          for(var i = 0; i < query_tokens.length; i++){
+            if(queryDatePublished(COMPLETE_DATA[book], query_tokens[i])) query_result.push(COMPLETE_DATA[book]);
+          }
+        }else if (COMPLETE_DATA[book][key]) {
+          var bk_value = COMPLETE_DATA[book][key].toString().toLowerCase();
+          for(var i = 0; i < query_tokens.length; i++){
+            if (bk_value.indexOf(query_tokens[i].toLowerCase()) > -1) {
+              query_result.push(COMPLETE_DATA[book]);
+            }
+          }
         }
       });
       return result;
+  }
+
+  function tokenizeQuery(){
+    if(query[0] == "\"" && query[query.length -1] == "\""){
+        query_tokens.push(query.replace(new RegExp("\"",'g'),""));
+      }else{
+        query_tokens = query.split(' ');        
+        query_tokens.push(query);
+        for(var i = 0; i < query_tokens.length; i++){
+          query_tokens[i] = query_tokens[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\\]/g,"");
+          query_tokens[i] = query_tokens[i].replace(/\s{2,}/g," ");
+        }
+      }
   }
 
   function queryCatalogue() {
       RESULTS_LIST.innerHTML = "";
       category = document.getElementsByName('category')[0].value;
       query = document.getElementsByName('query')[0].value;
+      query_tokens = [];
+      tokenizeQuery();
       filter_images = document.getElementById('query__check_filterImages').checked;
       query_result = [];
-      console.log()
+      
       if (category == "all") {
           Object.keys(COMPLETE_DATA).forEach(function(book){
               if(checkBookAttributes(book)) query_result.push(COMPLETE_DATA[book]);
           });
       } else {
+          // Specific Category Selected
           Object.keys(COMPLETE_DATA).forEach(function(book){
               Object.keys(COMPLETE_DATA[book]).forEach(function(key){
                   if (key.toString() == category) {
                       if(category == "date_published"){
-                        if(queryDatePublished(COMPLETE_DATA[book], query)) query_result.push(COMPLETE_DATA[book]);
+                        for(var i = 0; i < query_tokens.length; i++){
+                          if(queryDatePublished(COMPLETE_DATA[book], query_tokens[i])) query_result.push(COMPLETE_DATA[book]);
+                        }
                       }else if (COMPLETE_DATA[book][key]) {
-                          if (COMPLETE_DATA[book][key].toString().toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                          var bk_value = COMPLETE_DATA[book][key].toString().toLowerCase();
+                          for(var i = 0; i < query_tokens.length; i++){
+                            if (bk_value.indexOf(query_tokens[i].toLowerCase()) > -1) {
                               query_result.push(COMPLETE_DATA[book]);
+                            }
                           }
                       }
                   }
               });
           });
       }
+      
+      // remove duplicate entries
+      var flags = [], output = [];
+      for(var i = 0; i < query_result.length; i++) {
+        if( flags[query_result[i].book_id]) continue;
+        flags[query_result[i].book_id] = true;
+        output.push(query_result[i]);
+      }
+      query_result = output;
+      
       //sort by book id
       query_result.sort(function(a,b){
-        return a.book_id - b.book_id;
+          return a.book_id - b.book_id;
       });
 
       if(filter_images){ getFilterImages(); }
@@ -105,12 +144,12 @@
               Object.keys(bk).forEach(function(attr){
                   if (bk[attr]) {
                       if (category == "all") {
-                          bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category'>" + attr.replace("_", " ") + "</span>: " + highlightQuery(bk[attr], query) + "</p>";
+                          bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category'>" + attr.replace("_", " ") + "</span>: " + highlightQuery(bk[attr]) + "</p>";
                       } else if (category == attr) {
                           if(category == 'book_id'){
-                            bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category query__highlight-category'>Catalogue ID</span>: " + highlightQuery(bk[attr], query) + "</p>";
+                            bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category query__highlight-category'>Catalogue ID</span>: " + highlightQuery(bk[attr]) + "</p>";
                           }else{
-                            bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category query__highlight-category'>" + attr.replace("_", " ") + "</span>: " + highlightQuery(bk[attr], query) + "</p>";
+                            bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category query__highlight-category'>" + attr.replace("_", " ") + "</span>: " + highlightQuery(bk[attr]) + "</p>";
                           }
                       } else {
                           bookInfo += "<p class='query_result__" + attr + "'><span class='query_result__category'>" + attr.replace("_", " ") + "</span>: " + bk[attr] + "</p>";
@@ -135,16 +174,20 @@
       }, 600);
   }
 
-  function highlightQuery(string, query) {
-      var regex = new RegExp(query, "gi");
-      return string.toString().replace(regex, '<span class="query__highlight">' + query + '</span>');
+  function highlightQuery(string) {
+      var output = string;
+      for(var i = 0; i < query_tokens.length; i++){
+        var regex = new RegExp(query_tokens[i], "gi");
+        output = output.toString().replace(regex, '<span class="query__highlight">' + query_tokens[i] + '</span>');
+      }      
+      return output;
   }
   function showMoreResults() {
       var count = total_results_shown + number_displayed_results;
       if (count > query_result.length) {
           count = query_result.length;
       }
-      console.log("Total: " + total_results_shown + " Displayed: " + number_displayed_results + " Query Length: " + query_result.length + " count: " + count);
+      console.log("Total: " + total_results_shown + " ||Displayed: " + number_displayed_results + " ||Query Length: " + query_result.length + " ||count: " + count);
       RESULTS_CONTAINER.classList.add('query_hide-output');
       setTimeout(function() {
           for (var i = total_results_shown; i < count; i++) {
@@ -207,6 +250,7 @@
       };
     }
   }
+
   function queryDatePublished(bk,dateQuery){
     var parsed_date = findDateRange(bk.date_published);
     switch(parsed_date.type){
